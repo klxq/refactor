4b27dac58c7b202f5651458f5e255aeb39011635
wl.klxq@gmail.com <wl.klxq@gmail.com>
2017-11-27 23:33:13 +0800

    Extract method

--- src/main/Customer.java
+++ src/main/Customer.java
@@ -20,14 +20,7 @@ public class Customer {
         while (rentals.hasMoreElements()) {
             Rental rental = (Rental) rentals.nextElement();
 
-            // add frequent renter points
-            frequentRenterPoints++;
-
-            // add bonus for a two day new release rental
-            if((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-                    rental.getDaysRented() > 1) {
-                frequentRenterPoints++;
-            }
+            frequentRenterPoints = getFrequentRenterPoints(frequentRenterPoints, rental);
 
             // show figures for this rental
             result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(rental.getCharge()) + "\n";
@@ -39,6 +32,18 @@ public class Customer {
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
 
         return result;
+    }
+
+    private int getFrequentRenterPoints(int frequentRenterPoints, Rental rental) {
+        // add frequent renter points
+        frequentRenterPoints++;
+
+        // add bonus for a two day new release rental
+        if((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+                rental.getDaysRented() > 1) {
+            frequentRenterPoints++;
+        }
+        return frequentRenterPoints;
     }
 
     private double amountFor(Rental aRental) {
